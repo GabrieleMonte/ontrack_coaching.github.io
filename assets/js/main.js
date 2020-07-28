4325dd37ae7089d36735689d27741c752d3c280e
Gabriele Montefalcone <52413698+GabrieleMonte@users.noreply.github.com>
2020-07-28 00:10:06 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -264,10 +264,3 @@
 
 
 })(jQuery);
-
-	document.querySelector('.bg-modal').style.display = "flex";
-});
-
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
-});
